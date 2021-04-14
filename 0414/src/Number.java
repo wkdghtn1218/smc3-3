e31bf6c2f27144a5b1af1d4c35d3bdca1ba1dd61
kkh <sbs554342@naver.com>
2021-04-14 15:16:21 +0900

    0414 프로젝트 생성

---
+++ 0414/src/Number.java
@@ -0,0 +1,31 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Number {
+
+	public static void main(String[] args) {
+		
+		System.out.print("10개의 랜덤 숫자 중 하나를 맞춰보세요(1~50)까지:");
+		Scanner sc = new Scanner(System.in);
+		int num=sc.nextInt();
+		int diff = 0;
+		int []arr=new int[10];// 10개 공간생성
+		
+		for(int a=0; a<10; a++) { 
+			arr[a]=(int)(Math.random()*50+1); //숫자 생성 Random 함수 *마지막숫자+첫번째숫자
+			System.out.print(arr[a]+" ");
+		}
+		System.out.println();
+		
+		for(int i=0; i<10; i++) {
+			if(arr[i] == num) {
+				System.out.println("당첨!");
+				diff = 1;
+			}
+		}
+		if(diff == 0) System.out.println("꽝");
+	}
+}
+		
+		
+
