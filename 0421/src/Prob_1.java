de0de6d77963917a581b1f9e351194a6cb9e763c
wkdghtn1218 <hosoochang3130@naver.com>
2021-04-21 15:24:24 +0900

    0421 프로젝트 완성

---
+++ 0421/src/Prob_1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Prob_1 {
+
+		public static void main(String[] args) {
+
+			Scanner scan = new Scanner(System.in);
+			
+			System.out.print("시작 숫자: ");
+			int a = scan.nextInt();
+			System.out.print("마지막 숫자: ");
+			int b = scan.nextInt();
+			int sum = 0;
+			
+			
+			for(int i=a; i<=b; i++){
+				sum = sum + i;
+			
+			}
+			System.out.println(a + "부터 " + b + "까지의 합은 " + sum);
+		}
+
+	}
